972e62174884b762cdddbee33e74f517f8dcb5aa
jacobadam <jacobanevitt@gmail.com>
2024-06-07 10:39:13 -0700

    add folders

--- src/App.js
+++ src/App.js
@@ -1,7 +1,30 @@
-import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Layout from "./Layout.js";
+import Home from "./Home.js";
+import About from "./About.js";
+import Contact from "./Contact.js";
+import Conditions from "./Conditions.js";
+import Scope from "./Scope.js";
+import Location from "./Location.js";
+import Links from "./Links.js";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<Home />} />
+            <Route path="blogs" element={<About />} />
+            <Route path="conditions" element={<Conditions />} />
+            <Route path="scope-of-practice" element={<Scope />} />
+            <Route path="contact" element={<Contact />} />
+            <Route path="location" element={<Location />} />
+            <Route path="links" element={<Links />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
-
 export default App;
---
+++ src/Layout.js
@@ -0,0 +1,35 @@
+import React from "react";
+import { Outlet, Link } from "react-router-dom";
+
+export default function Layout() {
+  return (
+    <>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/blogs">About</Link>
+          </li>
+          <li>
+            <Link to="/conditions">Conditions</Link>
+          </li>
+          <li>
+            <Link to="/scope-of-practice">Scope</Link>
+          </li>
+          <li>
+            <Link to="/contact">Contact</Link>
+          </li>
+          <li>
+            <Link to="/location">Location</Link>
+          </li>
+          <li>
+            <Link to="/links">Links</Link>
+          </li>
+        </ul>
+      </nav>
+      <Outlet />
+    </>
+  );
+}
