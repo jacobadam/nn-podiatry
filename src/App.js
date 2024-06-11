d4df557420cc6c36532e7f21cb21bad01c80c882
jacobadam <jacobanevitt@gmail.com>
2024-06-11 14:27:18 -0700

    add tailwind nav and customise, restructure routing, remove unneeded css and rename folder

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Home from "./Home.js";
 import About from "./About.js";
 import Contact from "./Contact.js";
 import Conditions from "./Conditions.js";
-import Scope from "./Scope.js";
+import Podiatry from "./Podiatry.js";
 import Location from "./Location.js";
 import Links from "./Links.js";
 
@@ -12,17 +12,17 @@ function App() {
   return (
     <div>
       <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Nav />}>
-            <Route index element={<Home />} />
+        <Nav>
+          <Routes>
+            <Route path="/" element={<Home />} />
             <Route path="about" element={<About />} />
             <Route path="conditions" element={<Conditions />} />
-            <Route path="scope-of-practice" element={<Scope />} />
+            <Route path="podiatry" element={<Podiatry />} />
             <Route path="contact" element={<Contact />} />
             <Route path="location" element={<Location />} />
             <Route path="links" element={<Links />} />
-          </Route>
-        </Routes>
+          </Routes>
+        </Nav>
       </BrowserRouter>
     </div>
   );
--- src/podiatry-information.js
+++ src/podiatry-information.js
@@ -1,4 +1,4 @@
-const scopeInformation = [
+const podiatryInformation = [
   "Podiatry (or chiropody) is the medical speciality of caring for feet. (The terms podiatry and chiropody are interchangeable, although there are a growing number of surgical podiatrists in the UK who actually carry out ambulatory foot surgery on patients to treat conditions such as bunions and hammer toes).",
 
   "Podiatrists treat conditions on the feet such as hard skin, corns, nails (and in particular, are specialists in treating ingrowing toenails which can be cured by way of a minor surgical procedure under a local anaesthetic - a procedure that can be carried out in the surgery and takes less than an hour). We also treat verrucae although the present advice is to leave verrucae alone unless they are causing pain (this is following the recommendations of the BMA) - painful verrucae can be treated with chemical cautery by podiatrists.",
@@ -8,4 +8,4 @@ const scopeInformation = [
   "Evening appointments are also available.",
 ];
 
-export default scopeInformation;
+export default podiatryInformation;
