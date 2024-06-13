4497dd08c2deb5b7374ef18ba70c8014164e41fc
jacobadam <jacobanevitt@gmail.com>
2024-06-12 18:01:38 -0700

    update about me page and add image

--- src/About.js
+++ src/About.js
@@ -3,24 +3,54 @@ import aboutMeData from "./history";
 
 export default function About() {
   return (
-    <div>
-      <h1>About Me</h1>
-      <ul>
-        {aboutMeData.map((item) => (
-          <li key={item.id}>
-            {item.year && <strong>{item.year}:</strong>} {item.description}
-          </li>
-        ))}
-      </ul>
-      <div>
-        MCPod - Member of the College of Podiatry - membership number - 07675
+    <section class="min-h-screen bg-white dark:bg-gray-900">
+      <div class="py-8 px-4 mx-auto max-w-screen-xl lg:grid lg:grid-cols-2 lg:py-16 lg:px-6">
+        <div class="text-center lg:text-left lg:pt-8">
+          <h2 class="mb-4 text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white">
+            Neil Nevitt
+          </h2>
+          <div class="font-light text-gray-500 sm:text-lg dark:text-gray-400">
+            <p class="mb-4">
+              We are strategists, designers and developers. Innovators and
+              problem solvers. Small enough to be simple and quick, but big
+              enough to deliver the scope you want at the pace you need. Small
+              enough to be simple and quick, but big enough to deliver the scope
+              you want at the pace you need.
+            </p>
+            <p>
+              We are strategists, designers and developers. Innovators and
+              problem solvers. Small enough to be simple and quick.
+            </p>
+          </div>
+        </div>
+        <div class="flex justify-center items-center mt-8 lg:mt-0">
+          <img
+            class="w-48 sm:w-64 md:w-60 rounded-lg"
+            src="/neilnevitt.webp"
+            alt="neil nevitt"
+          />
+        </div>
       </div>
-      <div>
-        DPodM - Diploma of Podiatric Medicine - membership number M/07675
-      </div>
-      <div>
-        HCPC - Health Care Professional Council - membership number CH06728
-      </div>
-    </div>
+    </section>
   );
 }
+
+// <div>
+//   <h1>About Me</h1>
+//   <ul>
+//     {aboutMeData.map((item) => (
+//       <li key={item.id}>
+//         {item.year && <strong>{item.year}:</strong>} {item.description}
+//       </li>
+//     ))}
+//   </ul>
+//   <div>
+//     MCPod - Member of the College of Podiatry - membership number - 07675
+//   </div>
+//   <div>
+//     DPodM - Diploma of Podiatric Medicine - membership number M/07675
+//   </div>
+//   <div>
+//     HCPC - Health Care Professional Council - membership number CH06728
+//   </div>
+// </div>
