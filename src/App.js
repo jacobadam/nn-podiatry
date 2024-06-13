9e68eba06e9c7e7216a35f43f487aa0c755fb6a4
jacobadam <jacobanevitt@gmail.com>
2024-06-12 18:03:30 -0700

    remove podiatry component

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import Home from "./Home.js";
 import About from "./About.js";
 import Contact from "./Contact.js";
 import Conditions from "./Conditions.js";
-import Podiatry from "./Podiatry.js";
 import Location from "./Location.js";
 import Links from "./Links.js";
 
@@ -17,7 +16,6 @@ function App() {
             <Route path="/" element={<Home />} />
             <Route path="about" element={<About />} />
             <Route path="conditions" element={<Conditions />} />
-            <Route path="podiatry" element={<Podiatry />} />
             <Route path="contact" element={<Contact />} />
             <Route path="location" element={<Location />} />
             <Route path="links" element={<Links />} />
