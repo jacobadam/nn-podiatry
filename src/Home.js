cd3fc9949fd52ee1512c991f50beaa910c6c6fdd
jacobadam <jacobanevitt@gmail.com>
2024-06-14 15:59:25 -0700

    add extra copy

--- src/Home.js
+++ src/Home.js
@@ -16,6 +16,10 @@ export default function Home() {
           </h1>
 
           <p className="mt-4 max-w-lg sm:text-xl/relaxed">
+            <strong>HCPC (State) Registered</strong>
+          </p>
+
+          <p className="mt-4 max-w-lg sm:text-xl/relaxed">
             Experienced podiatrist with a comprehensive <br />
             background in foot health
           </p>
@@ -31,39 +35,5 @@ export default function Home() {
         </div>
       </div>
     </section>
-
-    /*
-    <section class="bg-gray-600">
-      <div class="grid max-w-screen-xl px-4 py-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12 bg-gray-800">
-        <div class=" place-self-center lg:col-span-7 ">
-          <h1 class="max-w-2xl mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl xl:text-6xl dark:text-black">
-            Neil Nevitt
-          </h1>
-          <p class="max-w-2xl mb-6 font-light text-gray-500 lg:mb-8 md:text-lg lg:text-xl dark:text-gray-400">
-            DPodM, MRCPod <br />
-            Liverpool Podiatrist/Chiropodist <br />
-            (Qualified Salford University 1976) <br />
-            HCPC (State) Registered <br />
-            Tel: 0151-722 3840 Mobile: 07946 582000 <br />
-            (Wavertree/Childwall)
-          </p>
-
-          <Link to="/contact">
-            <button class="inline-flex items-center justify-center px-5 py-3 text-base font-medium text-center text-gray-900 border border-gray-300 rounded-lg hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 dark:text-black dark:border-gray-700 dark:hover:bg-gray-200 dark:focus:ring-gray-200">
-              Get In Touch
-            </button>
-          </Link>
-        </div>
-        <div class="hidden lg:mt-0 lg:col-span-5 lg:flex w-full">
-          <img src="/homepage-image.webp" alt="homepage-stock" />
-        </div>
-      </div>
-      <div className="payment-qualification-images">
-        <img src="/accepted-cards.webp" alt="accepted-cards" />
-        <img src="/hcpc-reg.webp" alt="hcpc-reg" />
-      </div>
-    </section>
-    };
-  */
   );
 }
