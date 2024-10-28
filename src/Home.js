08dba8254f3b22c7ead59964f5b4485d37f2ecaa
jacobadam <jacobanevitt@gmail.com>
2024-10-28 14:35:50 -0700

    pre load hero image

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,12 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Link } from "react-router-dom";
 
 export default function Home() {
+  useEffect(() => {
+    const preloadImage = new Image();
+    preloadImage.src = "../public/homepage-image.webp";
+  }, []);
+
   return (
     <section
       className="relative bg-[url('../public/homepage-image.webp')] bg-cover bg-center bg-no-repeat bg-transparent"
