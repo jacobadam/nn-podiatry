e004fc38d4dbffe7252190e980d5c07c8db7a334
jacobadam <jacobanevitt@gmail.com>
2024-10-29 12:20:53 -0700

    add lazy load for performance

--- src/App.js
+++ src/App.js
@@ -1,28 +1,33 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import React, { Suspense, lazy } from "react";
 import Nav from "./Nav.js";
-import Home from "./Home.js";
-import About from "./About.js";
-import Contact from "./Contact.js";
-import Services from "./Services.js";
-import Location from "./Location.js";
-import Podiatry from "./Podiatry.js";
-import NotFound from "./NotFound.js";
 import RedirectHandler from "./RedirectHandler.js";
+import { BounceLoader } from "react-spinners";
+
+const Home = lazy(() => import("./Home.js"));
+const About = lazy(() => import("./About.js"));
+const Contact = lazy(() => import("./Contact.js"));
+const Services = lazy(() => import("./Services.js"));
+const Location = lazy(() => import("./Location.js"));
+const Podiatry = lazy(() => import("./Podiatry.js"));
+const NotFound = lazy(() => import("./NotFound.js"));
 
 function App() {
   return (
     <BrowserRouter>
       <RedirectHandler />
       <Nav>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="about" element={<About />} />
-          <Route path="services" element={<Services />} />
-          <Route path="podiatry" element={<Podiatry />} />
-          <Route path="contact" element={<Contact />} />
-          <Route path="location" element={<Location />} />
-          <Route path="*" element={<NotFound />} />
-        </Routes>
+        <Suspense fallback={<BounceLoader color={"#ffffff"} size={50} />}>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="about" element={<About />} />
+            <Route path="services" element={<Services />} />
+            <Route path="podiatry" element={<Podiatry />} />
+            <Route path="contact" element={<Contact />} />
+            <Route path="location" element={<Location />} />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </Suspense>
       </Nav>
     </BrowserRouter>
   );
