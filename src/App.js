e1926a5650d4632a9b6bf2f5580220b4db84cd68
jacobadam <jacobanevitt@gmail.com>
2025-01-12 10:27:33 -0800

    remove lazy load

--- src/App.js
+++ src/App.js
@@ -1,31 +1,27 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import React, { Suspense, lazy } from "react";
+import React from "react";
 import Nav from "./Nav.js";
-import { BounceLoader } from "react-spinners";
-
-const Home = lazy(() => import("./Home.js"));
-const About = lazy(() => import("./About.js"));
-const Contact = lazy(() => import("./Contact.js"));
-const Services = lazy(() => import("./Services.js"));
-const Location = lazy(() => import("./Location.js"));
-const Podiatry = lazy(() => import("./Podiatry.js"));
-const NotFound = lazy(() => import("./NotFound.js"));
+import Home from "./Home.js";
+import About from "./About.js";
+import Contact from "./Contact.js";
+import Services from "./Services.js";
+import Location from "./Location.js";
+import Podiatry from "./Podiatry.js";
+import NotFound from "./NotFound.js";
 
 function App() {
   return (
     <BrowserRouter>
       <Nav>
-        <Suspense fallback={<BounceLoader color={"#ffffff"} size={50} />}>
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="about" element={<About />} />
-            <Route path="services" element={<Services />} />
-            <Route path="podiatry" element={<Podiatry />} />
-            <Route path="contact" element={<Contact />} />
-            <Route path="location" element={<Location />} />
-            <Route path="*" element={<NotFound />} />
-          </Routes>
-        </Suspense>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="about" element={<About />} />
+          <Route path="services" element={<Services />} />
+          <Route path="podiatry" element={<Podiatry />} />
+          <Route path="contact" element={<Contact />} />
+          <Route path="location" element={<Location />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
       </Nav>
     </BrowserRouter>
   );
