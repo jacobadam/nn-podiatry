21c454076faccdfa081392724c13d5f0ce23f2f3
jacobadam <jacobanevitt@gmail.com>
2025-03-17 09:00:07 -0700

    remove service worker

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,11 +1,11 @@
-self.addEventListener("install", (event) => {
-  console.log("Service Worker: Installing...");
-});
+// self.addEventListener("install", (event) => {
+//   console.log("Service Worker: Installing...");
+// });
 
-self.addEventListener("activate", (event) => {
-  console.log("Service Worker: Activated");
-});
+// self.addEventListener("activate", (event) => {
+//   console.log("Service Worker: Activated");
+// });
 
-self.addEventListener("fetch", (event) => {
-  console.log("Service Worker: Fetching", event.request.url);
-});
+// self.addEventListener("fetch", (event) => {
+//   console.log("Service Worker: Fetching", event.request.url);
+// });
