c56ffd6624a78509071ad25cd8369604f11be951
jacobadam <jacobanevitt@gmail.com>
2025-04-28 13:11:48 -0600

    redo nav using Nav Link

--- src/Nav.js
+++ src/Nav.js
@@ -1,119 +1,121 @@
-import React, { useRef } from "react";
-import {
-  Disclosure,
-  DisclosureButton,
-  DisclosurePanel,
-} from "@headlessui/react";
-import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline";
+import React, { useState } from "react";
 import { NavLink, useLocation } from "react-router-dom";
-import ScrollToTop from "./ScrollToTop";
 
 const navigation = [
-  { name: "Home", href: "/", current: false },
-  { name: "About", href: "/about", current: false },
-  { name: "Services", href: "/services", current: false },
-  { name: "Podiatry", href: "/podiatry", current: false },
-  { name: "Contact", href: "/contact", current: false },
-  { name: "Location", href: "/location", current: false },
+  { label: "Home", to: "/" },
+  { label: "About", to: "/about" },
+  { label: "Services", to: "/services" },
+  { label: "Podiatry", to: "/podiatry" },
+  { label: "Contact", to: "/contact" },
+  { label: "Location", to: "/location" },
 ];
 
-export default function Nav(props) {
-  const buttonRef = useRef(null);
+export default function Nav() {
   const location = useLocation();
   const isHomePage = location.pathname === "/";
+  const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
+
+  const toggleMobileMenu = () => {
+    setIsMobileMenuOpen(!isMobileMenuOpen);
+  };
+
+  const scrollToTop = () => {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+    setIsMobileMenuOpen(false);
+  };
 
   return (
     <>
-      <ScrollToTop />
-      <Disclosure as="nav" className="bg-gray-800 w-full z-10">
-        {({ open }) => (
-          <>
-            <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
-              <div className="relative flex h-16 items-center justify-between">
-                <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
-                  {/* Mobile menu button*/}
-                  <DisclosureButton
-                    ref={buttonRef}
-                    className="relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white"
-                    tabIndex="0"
-                  >
-                    <span className="sr-only">Open main menu</span>
-                    {open ? (
-                      <XMarkIcon className="block h-6 w-6" aria-hidden="true" />
-                    ) : (
-                      <Bars3Icon className="block h-6 w-6" aria-hidden="true" />
-                    )}
-                  </DisclosureButton>
-                </div>
-                <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-center">
-                  <div className="hidden sm:ml-6 sm:block">
-                    <div className="flex space-x-4">
-                      {navigation.map((item) => (
-                        <NavLink
-                          key={item.name}
-                          to={item.href}
-                          className={({ isActive }) => {
-                            return (
-                              "rounded-md px-2 py-2 text-sm font-medium " +
-                              (isActive
-                                ? "bg-gray-700 text-white"
-                                : "text-gray-300 hover:bg-gray-700 hover:text-white")
-                            );
-                          }}
-                          aria-current={item.current ? "page" : undefined}
-                          tabIndex="0"
-                        >
-                          {item.name}
-                        </NavLink>
-                      ))}
-                    </div>
-                  </div>
-                </div>
-              </div>
+      <nav className="py-5 lg:fixed w-full bg-white transition-all z-50 sticky top-0 duration-500">
+        <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+          <div className="hidden lg:flex items-center justify-between w-full">
+            <div className="flex-1 flex items-center">
+              <a href="/">
+                <img
+                  src="/favicon.ico"
+                  alt="Your Logo"
+                  className="h-8 w-auto"
+                />
+              </a>
             </div>
 
-            <DisclosurePanel className="sm:hidden">
-              <div className="space-y-1 px-2 pb-3 pt-2">
-                {navigation.map((item) => (
-                  <NavLink
-                    key={item.name}
-                    to={item.href}
-                    className={({ isActive }) => {
-                      return (
-                        "block rounded-md px-3 py-2 text-base font-medium " +
-                        (isActive
-                          ? "bg-gray-700 text-white"
-                          : "text-gray-300 hover:bg-gray-700 hover:text-white")
-                      );
-                    }}
-                    aria-current={item.current ? "page" : undefined}
-                    tabIndex="0"
-                    onClick={() => {
-                      if (buttonRef.current) {
-                        buttonRef.current.click();
-                      }
-                    }}
-                  >
-                    {item.name}
-                  </NavLink>
-                ))}
-              </div>
-            </DisclosurePanel>
-          </>
-        )}
-      </Disclosure>
+            <div className="flex gap-8">
+              {navigation.map((item) => (
+                <NavLink
+                  key={item.to}
+                  to={item.to}
+                  onClick={scrollToTop}
+                  className={({ isActive }) =>
+                    isActive
+                      ? "text-cyan-600 font-semibold"
+                      : "text-black hover:text-cyan-600 transition font-medium"
+                  }
+                >
+                  {item.label}
+                </NavLink>
+              ))}
+            </div>
+
+            <div className="flex-1 hidden lg:flex flex-col items-end text-right text-sm leading-tight">
+              <span className="font-semibold text-cyan-600">
+                0151 722 3840/
+              </span>
+              <span className="font-semibold text-cyan-600">07595 373 585</span>
+            </div>
+          </div>
+
+          <div className="flex lg:hidden justify-between items-center">
+            <a href="/">
+              <img src="/favicon.ico" alt="Your Logo" className="h-8 w-auto" />
+            </a>
+            <button
+              onClick={toggleMobileMenu}
+              className="inline-flex items-center p-2 text-sm text-gray-500 rounded-lg hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200"
+            >
+              <svg
+                className="w-6 h-6"
+                aria-hidden="true"
+                fill="currentColor"
+                viewBox="0 0 20 20"
+              >
+                <path
+                  fillRule="evenodd"
+                  d="M3 5h14a1 1 0 100-2H3a1 1 0 100 2zm14 4H3a1 1 0 000 2h14a1 1 0 000-2zm0 6H3a1 1 0 000 2h14a1 1 0 000-2z"
+                  clipRule="evenodd"
+                />
+              </svg>
+            </button>
+          </div>
+
+          {isMobileMenuOpen && (
+            <div className="lg:hidden mt-4 flex flex-col gap-4">
+              {navigation.map((item) => (
+                <NavLink
+                  key={item.to}
+                  to={item.to}
+                  onClick={scrollToTop}
+                  className={({ isActive }) =>
+                    isActive
+                      ? "text-cyan-600 font-semibold"
+                      : "text-black hover:text-cyan-600 transition font-medium"
+                  }
+                >
+                  {item.label}
+                </NavLink>
+              ))}
+            </div>
+          )}
+        </div>
+      </nav>
       {!isHomePage && (
-        <div className="w-full h-40">
+        <div className="w-full h-60 lg:mt-20">
           <img
             src={`${process.env.PUBLIC_URL}/pebble-footprint.webp`}
             alt="Banner"
             className="w-full h-full object-cover"
-            aria-label="Banner Image"
           />
         </div>
       )}
-
-      <div className="bg-white min-h-screen">{props.children}</div>
     </>
   );
 }
