690bef2307e7cdd985311f049238acfdbb0a7661
jacobadam <jacobanevitt@gmail.com>
2025-04-29 10:31:44 -0600

    update footer position

--- src/Footer.js
+++ src/Footer.js
@@ -5,7 +5,7 @@ const Footer = () => {
 
   return (
     <footer className="bg-blue-950 text-gray-300 py-6">
-      <div className="container mx-auto px-4">
+      <div className="pl-8">
         <div className="text-center md:text-left text-sm">
           &copy; {currentYear} Neil Nevitt. All rights reserved.
         </div>
