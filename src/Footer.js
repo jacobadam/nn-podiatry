e493ee42903863151c66cc9195a412ea9c8158a0
jacobadam <jacobanevitt@gmail.com>
2025-06-04 15:41:14 -0600

    update copyright

--- src/Footer.js
+++ src/Footer.js
@@ -7,7 +7,7 @@ const Footer = () => {
     <footer className="bg-blue-950 text-gray-300 py-6" role="contentinfo">
       <div className="pl-8">
         <p className="text-center md:text-left text-sm">
-          &copy; {currentYear} Neil Nevitt. All rights reserved.
+          &copy; {currentYear} Greenhouse Web Designs. All rights reserved.
         </p>
       </div>
     </footer>
